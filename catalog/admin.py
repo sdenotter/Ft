3a6eb611f761d16f167afe528a39f6261a8bee6e
unknown <scott.denotter1@gmail.com>
2018-03-09 09:54:48 -0500

    First

---
+++ catalog/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from .models import Author, Genre, Book, BookInstance
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+#admin.site.register(BookInstance)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+# Register the Admin classes for BookInstance using the decorator
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
---
+++ catalog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+# Create your views here.
+def index(request):
+    """
+
+   Home page view function
+    """
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
+
+    return render (
+        request, 'index.html', context={'num_books': num_books ,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+
+    )
